e8cb24e907bc12c00452449dfc3026f4270d8250
MasatoMakino <unagiinu.g@gmail.com>
2019-08-28 21:41:06 +0900

    add : default prop.

--- src/postprocess/PostProcessShaderPass.ts
+++ src/postprocess/PostProcessShaderPass.ts
@@ -1,6 +1,14 @@
 import { IUniform } from "three";
 import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+import { Texture } from "three";
 
 export class PostProcessShaderPass extends ShaderPass {
   uniforms: { [uniform: string]: IUniform };
+
+  get tDiffuse(): Texture {
+    return this.uniforms.tDiffuse.value;
+  }
+  set tDiffuse(value: Texture) {
+    this.uniforms.tDiffuse.value = value;
+  }
 }
