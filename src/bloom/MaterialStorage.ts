7a5c40ebb590dc64a3f1e40e38232c902d498f82
MasatoMakino <unagiinu.g@gmail.com>
2019-08-31 10:59:49 +0900

    add : material array support.

--- src/bloom/MaterialStorage.ts
+++ src/bloom/MaterialStorage.ts
@@ -5,29 +5,92 @@ import { Material } from "three";
  * Object3D.userData.materialStorageに格納される。
  */
 export class MaterialStorage {
-  original?: Material;
-  darken?: Material;
+  original?: Material | Material[];
+  dark?: Material | Material[];
 
-  public updateMaterial(original: Material) {
+  public updateMaterial(original: Material | Material[]) {
     this.original = original;
 
-    if (this.darken == null || this.darken.type !== this.original.type) {
-      this.darken = this.original.clone();
+    this.copyMaterialArray();
+    this.darkenMaterialArray(this.dark);
+  }
+
+  private copyMaterialArray(): void {
+    if (this.isClone()) {
+      this.cloneToDark();
     } else {
-      this.darken.copy(this.original);
+      this.copyToDark();
+    }
+  }
+
+  /**
+   * darkenマテリアルのコピーに、クローンを使用するかcopyを使用するかを判定する。
+   */
+  private isClone(): boolean {
+    const darkenHead: Material = this.getHeadMaterial(this.dark);
+    const originalHead: Material = this.getHeadMaterial(this.original);
+
+    if (darkenHead == null || darkenHead.type !== originalHead.type) {
+      return true;
+    }
+    return false;
+  }
+
+  private getHeadMaterial(mat: Material | Material[]): Material {
+    const isArray = Array.isArray(mat);
+    if (isArray) return mat[0];
+    return mat as Material;
+  }
+
+  private copyToDark(): void {
+    const isArrayOriginal = Array.isArray(this.original);
+    if (!isArrayOriginal) {
+      (this.dark as Material[]).forEach((drk, index) => {
+        drk.copy(this.original[index]);
+      });
+    } else {
+      (this.dark as Material).copy(this.original as Material);
+    }
+  }
+
+  private cloneToDark(): void {
+    const isArrayOriginal = Array.isArray(this.original);
+    if (!isArrayOriginal) {
+      this.dark = (this.original as Material[]).map(val => {
+        return val.clone();
+      });
+    } else {
+      this.dark = (this.original as Material).clone();
+    }
+  }
+
+  private darkenMaterialArray(material: Material | Material[]): void {
+    if (!Array.isArray(material)) {
+      MaterialStorage.darkenMaterial(material);
+      return;
     }
 
-    if (this.darken["color"] != null) {
-      this.darken["color"].setHex(0);
+    material.forEach(mat => {
+      MaterialStorage.darkenMaterial(mat);
+    });
+  }
+
+  /**
+   * マテリアルを反射光のない黒に書き換える。
+   * @param material
+   */
+  private static darkenMaterial(material: Material): void {
+    if (material["color"] != null) {
+      material["color"].setHex(0);
     }
-    if (this.darken["shininess"] != null) {
-      this.darken["shininess"] = 0;
+    if (material["shininess"] != null) {
+      material["shininess"] = 0;
     }
-    if (this.darken["specular"] != null) {
-      this.darken["specular"].setHex(0);
+    if (material["specular"] != null) {
+      material["specular"].setHex(0);
     }
-    if (this.darken["emissive"] != null) {
-      this.darken["emissive"].setHex(0);
+    if (material["emissive"] != null) {
+      material["emissive"].setHex(0);
     }
   }
 }
