0b4153881d510aafcfd575b3b342e32167ce8958
MasatoMakino <MasatoMakino@users.noreply.github.com>
2024-06-26 02:58:00 +0000

    Build binary by github-actions

--- esm/postprocess/PostProcessShaderPass.js
+++ esm/postprocess/PostProcessShaderPass.js
@@ -1,9 +1,18 @@
 import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
+/**
+ * ポストプロセス用のShaderPass。
+ * EffectComposerにPassとして追加することで、任意のポストエフェクトを実現する。
+ *
+ * コンストラクターでPostProcessShaderを受け取り、そのシェーダーのuniformを操作する。
+ */
 export class PostProcessShaderPass extends ShaderPass {
-    get tDiffuse() {
-        return this.uniforms.tDiffuse.value;
-    }
-    set tDiffuse(value) {
-        this.uniforms.tDiffuse.value = value;
-    }
+  constructor(shader, textureID) {
+    super(shader, textureID);
+  }
+  get tDiffuse() {
+    return this.uniforms.tDiffuse.value;
+  }
+  set tDiffuse(value) {
+    this.uniforms.tDiffuse.value = value;
+  }
 }
